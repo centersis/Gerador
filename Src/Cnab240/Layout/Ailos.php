66607b2519054db8a7f2b71a0016dfa34769313e
Pablo Vanni <pablovanni@gmail.com>
2021-02-12 16:53:43 -0400

    ailos implementado

---
+++ Src/Cnab240/Layout/Ailos.php
@@ -0,0 +1,433 @@
+<?php
+
+namespace Gerador\Src\Cnab240\Layout;
+
+class Ailos
+{
+
+    public function headerArquivo()
+    {
+        return [
+            1 => [3, 'num'],
+            2 => [4, 'num'],
+            3 => [1, 'num'],
+            4 => [9, 'texto'],
+            5 => [1, 'num'],
+            6 => [14, 'doc'],
+            7 => [20, 'texto'],
+            8 => [5, 'num'],
+            9 => [1, 'texto'],
+            10 => [12, 'num'],
+            11 => [1, 'texto'],
+            12 => [1, 'texto'],
+            13 => [30, 'texto'],
+            14 => [30, 'texto'],
+            15 => [10, 'texto'],
+            16 => [1, 'num'],
+            17 => [8, 'data-d-m-Y'],
+            18 => [6, 'num'],
+            19 => [6, 'num'],
+            20 => [3, 'num'],
+            21 => [5, 'num'],
+            22 => [20, 'texto'],
+            23 => [20, 'texto'],
+            24 => [29, 'texto'],
+        ];
+    }
+
+    public function headerArquivoDefault()
+    {
+        return [
+            1 => 85,
+            2 => 0,
+            3 => 0,
+            4 => "",
+            5 => 2,
+            14 => "AILOS",
+            15 => "",
+            16 => 1,
+            17 => date('dmY'),
+            18 => date('His'),
+            20 => 87,
+            21 => 1600,
+            22 => "",
+            23 => "",
+            24 => ""
+        ];
+    }
+
+    public function headerArquivoValidacao()
+    {
+        return [
+            5 => "validaOpcao1e2",
+            6 => "validaCpfeCnpj",
+            16 => "validaHora",
+        ];
+    }
+
+    public function headerLote()
+    {
+        return [
+            1 => [3, 'num'],
+            2 => [4, 'num'],
+            3 => [1, 'num'],
+            4 => [1, 'texto'],
+            5 => [2, 'num'],
+            6 => [2, 'texto'],
+            7 => [3, 'num'],
+            8 => [1, 'texto'],
+            9 => [1, 'num'],
+            10 => [15, 'num'],
+            11 => [20, 'texto'],
+            12 => [5, 'num'],
+            13 => [1, 'texto'],
+            14 => [12, 'num'],
+            15 => [1, 'texto'],
+            16 => [1, 'texto'],
+            17 => [30, 'texto'],
+            18 => [40, 'texto'],
+            19 => [40, 'texto'],
+            20 => [8, 'num'],
+            21 => [8, 'data-dmY'],
+            22 => [8, 'data-dmY'],
+            23 => [33, 'texto']
+        ];
+    }
+
+    public function headerLoteDefault()
+    {
+        return [
+            1 => 85,
+            2 => 1,
+            3 => 1,
+            4 => "R",
+            5 => 1,
+            6 => "",
+            7 => 45,
+            8 => "",
+            9 => 2,
+            18 => "",
+            19 => "",
+            21 => date('Y-m-d'),
+            22 => 0,
+            23 => ""
+        ];
+    }
+
+    public function headerLoteValidacao()
+    {
+        return [];
+    }
+
+    public function segmentoP()
+    {
+        return [
+            1 => [3, 'num'],
+            2 => [4, 'num'],
+            3 => [1, 'num'],
+            4 => [5, 'num'],
+            5 => [1, 'texto'],
+            6 => [1, 'texto'],
+            7 => [2, 'num'],
+            8 => [5, 'num'],
+            9 => [1, 'texto'],
+            10 => [12, 'num'],
+            11 => [1, 'texto'],
+            12 => [1, 'texto'],
+            13 => [20, 'texto'],
+            14 => [1, 'num'],
+            15 => [1, 'num'],
+            16 => [1, 'texto'],
+            17 => [1, 'num'],
+            18 => [1, 'texto'],
+            19 => [15, 'texto'],
+            20 => [8, 'data-dmY'],
+            21 => [15, 'valor'],
+            22 => [5, 'num'],
+            23 => [1, 'texto'],
+            24 => [2, 'num'],
+            25 => [1, 'texto'],
+            26 => [8, 'data-dmY'],
+            27 => [1, 'num'],
+            28 => [8, 'data-dmY'],
+            29 => [15, 'vlor'],
+            30 => [1, 'num'],
+            31 => [8, 'data-dmY'],
+            32 => [15, 'valor'],
+            33 => [15, 'valor'],
+            34 => [15, 'valor'],
+            35 => [25, 'texto'],
+            36 => [1, 'num'],
+            37 => [2, 'num'],
+            38 => [1, 'num'],
+            39 => [3, 'texto'],
+            40 => [2, 'num'],
+            41 => [10, 'num'],
+            42 => [1, 'texto']
+        ];
+    }
+
+    public function segmentoPDefault()
+    {
+        return [
+            1 => 85,
+            2 => 1,
+            3 => 3,
+            5 => "P",
+            6 => "",
+            7 => 1,
+            15 => 1,
+            16 => 1,
+            17 => 2,
+            18 => 2,
+            24 => 2,
+            25 => 'A',
+            27 => 3,
+            28 => 0,
+            29 => 0,
+            30 => 0,
+            31 => 0,
+            32 => 0,
+            33 => 0,
+            34 => 0,
+            36 => 3,
+            37 => 0,
+            38 => 2,
+            39 => '',
+            40 => 9,
+            42 => ''
+        ];
+    }
+
+    public function segmentoPValidacao()
+    {
+        return [
+            /* 7 => "validaMovimentoRemessa",
+              17 => "validaOpcao1e2",
+              18 => "validaOpcao1e2",
+              24 => "validaEspecieTitulo",
+              25 => "validaIdTitulo",
+              28 => "validaJuroseDescData",
+              29 => "validaJurosDescValor",
+              27 => "validaCodigo",
+              //31 => "validaJuroseDescData",
+              32 => "validaJurosDescValor",
+              36 => "validaCodigoProtesto",
+              36 => "validaCodigoProteMovi",
+              40 => "codigoMoeda", */
+        ];
+    }
+
+    public function segmentoQ()
+    {
+        return [
+            1 => [3, 'num'],
+            2 => [4, 'num'],
+            3 => [1, 'num'],
+            4 => [5, 'num'],
+            5 => [1, 'texto'],
+            6 => [1, 'texto'],
+            7 => [2, 'num'],
+            8 => [1, 'num'],
+            9 => [15, 'num'],
+            10 => [40, 'texto'],
+            11 => [40, 'texto'],
+            12 => [15, 'texto'],
+            13 => [5, 'num'],
+            14 => [3, 'num'],
+            15 => [15, 'texto'],
+            16 => [2, 'texto'],
+            17 => [1, 'num'],
+            18 => [15, 'num'],
+            19 => [40, 'texto'],
+            20 => [3, 'num'],
+            21 => [20, 'texto'],
+            22 => [8, 'texto'],
+        ];
+    }
+
+    public function segmentoQDefault()
+    {
+        return [
+            1 => 85,
+            2 => 1,
+            3 => 3,
+            5 => "Q",
+            6 => "",
+            7 => 1,
+            20 => 0,
+            21 => ""
+        ];
+    }
+
+    public function segmentoQValidacao()
+    {
+        return [
+            /* 7 => "validaMovimentoRemessa",
+              8 => "validaOpcao1e2",
+              9 => "validaCpfeCnpj",
+              13 => "validaCep",
+              14 => "validaSufixoCep",
+              16 => "validaUf",
+              17 => "validaOpcao1e2",
+              18 => "validaCpfeCnpj", */
+        ];
+    }
+
+    public function segmentoR()
+    {
+        return [
+            1 => [3, 'num'],
+            2 => [4, 'num'],
+            3 => [1, 'num'],
+            4 => [5, 'num'],
+            5 => [1, 'texto'],
+            6 => [1, 'texto'],
+            7 => [2, 'num'],
+            8 => [1, 'num'],
+            9 => [8, 'data-dmY'],
+            10 => [8, 'valor'],
+            11 => [1, 'num'],
+            12 => [8, 'data-dmY'],
+            13 => [15, 'valor'],
+            14 => [1, 'texto'],
+            15 => [8, 'data-dmY'],
+            16 => [15, 'valor'],
+            17 => [10, 'texto'],
+            18 => [40, 'texto'],
+            19 => [40, 'texto'],
+            20 => [20, 'texto'],
+            21 => [8, 'num'],
+            22 => [3, 'num'],
+            23 => [5, 'num'],
+            24 => [1, 'texto'],
+            25 => [12, 'num'],
+            26 => [1, 'texto'],
+            27 => [1, 'texto'],
+            28 => [1, 'num'],
+            29 => [9, 'texto'],
+        ];
+    }
+
+    public function segmentoRDefault()
+    {
+        return [
+            1 => 85,
+            2 => 1,
+            3 => 3,
+            5 => "R",
+            6 => "",
+            7 => 1,
+            8 => 0,
+            9 => 0,
+            10 => 0,
+            11 => 0,
+            12 => 0,
+            13 => 0,
+            14 => 0,
+            15 => 0,
+            16 => 0,
+            17 => "",
+            18 => "",
+            19 => "",
+            20 => "",
+            21 => 0,
+            22 => 85,
+            28 => 2,
+            29 => ''
+        ];
+    }
+
+    public function segmentoRValidacao()
+    {
+        return [
+            /* 7 => "validaMovimentoRemessa",
+              8 => "validaCodigo",
+              //9 => "validaJuroseDescData",
+              10 => "validaJurosDescValor",
+              11 => "validaCodigo",
+              12 => "validaJuroseDescData",
+              13 => "validaJurosDescValor",
+              14 => "validaCodigo",
+              //15 => "validaJuroseDescData",
+              16 => "validaJurosDescValor", */
+        ];
+    }
+
+    public function trailerLote()
+    {
+        return [
+            1 => [3, 'num'],
+            2 => [4, 'num'],
+            3 => [1, 'num'],
+            4 => [9, 'texto'],
+            5 => [6, 'num'],
+            6 => [6, 'num'],
+            7 => [17, 'valor'],
+            8 => [6, 'num'],
+            9 => [17, 'valor'],
+            10 => [6, 'num'],
+            11 => [17, 'valor'],
+            12 => [6, 'num'],
+            13 => [17, 'valor'],
+            14 => [8, 'num'],
+            15 => [117, 'texto'],
+        ];
+    }
+
+    public function trailerLoteDefault()
+    {
+        return [
+            1 => 85,
+            2 => 1,
+            3 => 5,
+            4 => "",
+            8 => 0,
+            9 => 0,
+            10 => 0,
+            11 => 0,
+            12 => 0,
+            13 => 0,
+            14 => "",
+            15 => "",
+        ];
+    }
+
+    public function trailerLoteValidacao()
+    {
+        return [];
+    }
+
+    public function trailerArquivo()
+    {
+        return [
+            1 => [3, 'num'],
+            2 => [4, 'num'],
+            3 => [1, 'num'],
+            4 => [9, 'texto'],
+            5 => [6, 'num'],
+            6 => [6, 'num'],
+            7 => [6, 'num'],
+            8 => [205, 'texto'],
+        ];
+    }
+
+    public function trailerArquivoDefault()
+    {
+        return [
+            1 => 85,
+            2 => 9999,
+            3 => 9,
+            4 => "",
+            5 => 1,
+            7 => 0,
+            8 => "",
+        ];
+    }
+
+    public function trailerArquivoValidacao()
+    {
+        return [];
+    }
+
+}
---
+++ Testes/Cnab240/Ailos.php
@@ -0,0 +1,122 @@
+<?php
+
+//Apenas um teste
+function my_autoloader($class) {
+    include __DIR__ . '/../../../' . str_replace('\\', '/', $class) . '.php';
+}
+
+spl_autoload_register('my_autoloader');
+
+
+$instanciaCnab240 = new Gerador\Src\Cnab240\Cnab240();
+$headerArquivo = [
+    6 => 79885950000109,   
+    8 => 1104,
+    9 => 5,
+    10 => "185958",
+    11 => "ASSOCIACAO LOJA SIMBOLICA LEALDADE",
+    13 => "Centersis Tecnologia da Informação LTDA - ME",
+    14 => "Sicoob",
+    19 => 49,
+];
+$instanciaCnab240->setHeaderArquivo($headerArquivo);
+
+$headerLoteTitulo = [
+    2 => 1,
+    9=>0,
+    10 => 37455649000107,
+    11=>0,
+    12 => 4425, //Prefixo da cooperativa - Agencia
+    13 => 3, //Dígito verificador da agencia
+    14 => 2758, //Número da Conta Corrente
+    15 => 8, //Dígito da conta corrente
+    17 => 'Centersis', //Nome da Empresa
+    19=>0,
+    20 => 1, //Número da remessa
+    21=>0,
+    22=>0,
+    23=>0,
+    24=>0,
+    25=>0,
+];
+
+$instanciaCnab240->setHeaderLoteTitulo($headerLoteTitulo, 1);
+
+$segmentoJ = [
+    2 => 1,
+    4 => 00001,
+    7 => 1,
+    8 => 1,
+    9 => 9,
+    10 => 20150506,
+    11 => 11,
+    12=>0,
+    13 => "000002132001014",
+    14 => 20150506,
+    15=>0,
+    17 => 2,
+    18 => 2,
+    19 => 0,
+    20 => 22012019,
+    21 => 153.25,
+    24 => 02,
+    25 => "A",
+    27 => 2,
+    28 => 22012019,
+    29 => 100,
+    30 => 0,
+    35 => "BOLETO TESTE PARCELA UNICA",
+    36 => 1,
+    40 => 9,
+];
+$instanciaCnab240->setSegmentoJ($segmentoJ, 1);
+
+$segmentoJ52 = [
+    2 => 1,
+    4 => 00002,
+    7 => 01,
+    8 => 2,
+    9 => 0,
+    10 => "Grande Loja Maçonica do Amazonas - GLOMA",
+    11 => "Av. Prof Nilton Lins",
+    12 => "Parque das Laranjeiras - Flores",
+    13 => 69058,
+    14 => "030",
+    15 => "Manaus",
+    16 => "AM",
+    18 => '0',
+    19 => "CENTERSIS TECNOLOGIA DA INFORMACAO LTDA",
+    20 => 000,
+    21 => "",
+];
+$instanciaCnab240->setSegmentoJ52($segmentoJ52, 1);
+
+
+$trailerLoteTitulo = [
+    2 => 1,
+    6=>0,
+    5 => 5,
+];
+$instanciaCnab240->setTrailerLoteTitulo($trailerLoteTitulo,1);
+
+$trailerArquivo = [
+    6=>0,
+];
+$instanciaCnab240->setTrailerArquivo($trailerArquivo);
+
+
+try {
+
+
+    $instanciaCnab240->gerar('Ailos', '../../Storage', 'Ailos.txt', [
+        'case' => 'upper',
+        'obrigatorio' => ['headerArquivo', 'headerLoteTitulo', 'segmentoJ', 'segmentoJ52', 'trailerLoteTitulo', 'trailerArquivo']
+    ]);
+
+    echo "Arquivo Gerado " . microtime();
+} catch (\Exception $ex) {
+    echo "Erro: " . $ex->getMessage();
+    echo "<br>";
+    echo $ex->getTraceAsString();
+}
+
