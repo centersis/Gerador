5d7ca2f4689bd6f12d488c69e8fe33822de22825
pablovanni@gmail.com <pablovanni@gmail.com>
2021-10-05 17:33:27 -0400

    ajustes no tratamento de texto

--- Src/Arquivo/Util.php
+++ Src/Arquivo/Util.php
@@ -58,11 +58,11 @@ class Util
         $retorno = $this->removeAcentos(rtrim(html_entity_decode($texto)));
 
         if ($case == 'upper') {
-            return strtoupper($retorno);
+            return strtr(mb_strtoupper($retorno), "àáâãäåæçèéêëìíîïðñòóôõö÷øùüúþÿ", "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÜÚÞß");
         }
 
         if ($case == 'lower') {
-            return strtolower($retorno);
+            return strtr(mb_strtolower($retorno), "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÜÚÞß", "àáâãäåæçèéêëìíîïðñòóôõö÷øùüúþÿ");
         }
 
         return $retorno;
@@ -70,39 +70,20 @@ class Util
 
     private function removeAcentos($string)
     {
-        $string = str_replace(['ª','º'], '.', $string);
-        
-        return preg_replace(
-            [
-            '/\xc3[\x80-\x85]/',
-            '/\xc3\x87/',
-            '/\xc3[\x88-\x8b]/',
-            '/\xc3[\x8c-\x8f]/',
-            '/\xc3([\x92-\x96]|\x98)/',
-            '/\xc3[\x99-\x9c]/',
-            '/\xc3[\xa0-\xa5]/',
-            '/\xc3\xa7/',
-            '/\xc3[\xa8-\xab]/',
-            '/\xc3[\xac-\xaf]/',
-            '/\xc3([\xb2-\xb6]|\xb8)/',
-            '/\xc3[\xb9-\xbc]/',
-            ], str_split('ACEIOUaceiou', 1), $this->isUtf8($string) ? $string : utf8_encode($string)
-        );
-    }
+        $normalizeChars = array(
+            'Á' => 'A', 'À' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Å' => 'A', 'Ä' => 'A', 'Æ' => 'AE', 'Ç' => 'C',
+            'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Í' => 'I', 'Ì' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ð' => 'Eth',
+            'Ñ' => 'N', 'Ó' => 'O', 'Ò' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O',
+            'Ú' => 'U', 'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Ŕ' => 'R',
 
-    private function isUtf8($string)
-    {
-        return preg_match('%^(?:
-                 [\x09\x0A\x0D\x20-\x7E]
-                | [\xC2-\xDF][\x80-\xBF]
-                | \xE0[\xA0-\xBF][\x80-\xBF]
-                | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}
-                | \xED[\x80-\x9F][\x80-\xBF]
-                | \xF0[\x90-\xBF][\x80-\xBF]{2}
-                | [\xF1-\xF3][\x80-\xBF]{3}
-                | \xF4[\x80-\x8F][\x80-\xBF]{2}
-                )*$%xs', $string
+            'á' => 'a', 'à' => 'a', 'â' => 'a', 'ã' => 'a', 'å' => 'a', 'ä' => 'a', 'æ' => 'ae', 'ç' => 'c',
+            'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e', 'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'eth',
+            'ñ' => 'n', 'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o',
+            'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u', 'ý' => 'y', 'ŕ' => 'r', 'ÿ' => 'y',
+
+            'ß' => 'sz', 'þ' => 'thorn', 'º' => '', 'ª' => '', '°' => '',
         );
+        return preg_replace('/[^0-9a-zA-Z !*\-$\(\)\[\]\{\},.;:\/\\#%&@+=]/', '', strtr($string, $normalizeChars));
     }
 
     public function adicionarZerosEsq($str, $tamanho)
